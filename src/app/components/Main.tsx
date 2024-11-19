dc5a14006a92a8faa914d03f0b5c126be7eec2cf
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-11-18 19:10:21 -0500

    first commit

---
+++ src/app/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import Services from "./Services";
+import AboutUs from "./AboutUs";
+import Footer from "./Footer";
+
+const Main = () => {
+  return <section></section>;
+};
+
+export default Main;
